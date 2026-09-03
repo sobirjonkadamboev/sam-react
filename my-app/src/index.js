b06dc142ff1b513293978eb63cf7f07f72d11f8e
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-03 04:20:55 +0000

    Auto-backup: 9/3/2026, 4:20:55 AM - Updated my-app/src/index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const el = <h1>Heading element</h1>
+console.dir(el)
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
