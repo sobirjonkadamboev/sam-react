e7312dad14bd32c4a7b94aa256d237c9d58edfac
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-04 05:59:07 +0000

    Auto-backup: 9/4/2026, 5:59:07 AM - Updated my-app/src/components/app/App.js

--- my-app/src/components/app/App.js
+++ my-app/src/components/app/App.js
@@ -1,10 +1,15 @@
+import HeaderFunction from '../header/header'
 import './App.css'
 
 //har doim komponentlar katta harfda yoziladi
 //funksiyalar kichkina harfda yoziladi
 
 function App() {
-	return <div className='App'></div>
+	return (
+		<div className='App'>
+			<HeaderFunction />
+		</div>
+	)
 }
 
 export default App
