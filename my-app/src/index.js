8b2454c973238be2a29b4f3f0e8d5b4dc99a0131
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-04 03:24:05 +0000

    Auto-backup: 9/4/2026, 3:24:05 AM - Updated my-app/src/index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+const element = <h1>Heading Element</h1>
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>Hi, React</h1>)
+root.render(element)
