92a994043d6157b9fe3d0b2a2a5c41eff27ddc34
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-04 06:07:47 +0000

    Auto-backup: 9/4/2026, 6:07:47 AM - Updated my-app/src/index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,18 +2,5 @@ import ReactDOM from 'react-dom/client'
 import App from './components/app/App'
 import './index.css'
 
-/* const element = <h1>Heading Element</h1>
-
-const text = 'Hi, AI'
-
-const el = (
-	<div>
-		<h1 className='heading'>Heading 1</h1>
-		<h2>{text}</h2>
-		<input type='text' />
-		<button>Click</button>
-	</div>
-) */
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
