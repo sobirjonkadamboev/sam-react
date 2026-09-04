07365ed22634db1a97407d1425bf6810613aabaa
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-04 03:29:33 +0000

    Auto-backup: 9/4/2026, 3:29:33 AM - Updated my-app/src/index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,7 +3,13 @@ import './index.css'
 
 const element = <h1>Heading Element</h1>
 
-const el = <div></div>
+const el = (
+	<div>
+		<h1>Heading 1</h1>
+		<input type='text' />
+		<button>Button</button>
+	</div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(element)
