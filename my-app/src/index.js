79ccaccf7adfcfd5690521fb0178ac0c611b958b
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-04 03:40:28 +0000

    Auto-backup: 9/4/2026, 3:40:27 AM - Updated my-app/src/index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
 
 /* const element = <h1>Heading Element</h1>
@@ -15,4 +16,4 @@ const el = (
 ) */
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(el)
+root.render(<App />)
