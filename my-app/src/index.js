44810985c2254c18f7ec8c69b49571288c3077ea
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-04 03:35:02 +0000

    Auto-backup: 9/4/2026, 3:35:02 AM - Updated my-app/src/index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,9 +3,12 @@ import './index.css'
 
 //const element = <h1>Heading Element</h1>
 
+const text = 'Hi, AI'
+
 const el = (
 	<div>
 		<h1 className='heading'>Heading 1</h1>
+		<h2>{text}</h2>
 		<input type='text' />
 		<button>Click</button>
 	</div>
