cf751ca49666618e039419e145ca8c8317583bfc
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-09 11:05:25 +0000

    Auto-backup: 9/9/2026, 11:05:25 AM - Updated cine-blox/cine-blox/src/components/app-filter/app-filter.js

--- cine-blox/cine-blox/src/components/app-filter/app-filter.js
+++ cine-blox/cine-blox/src/components/app-filter/app-filter.js
@@ -1,7 +1,15 @@
 const AppFilter = () => {
 	return (
 		<div className='btn-group'>
-			<button className='btn btn-dark'></button>
+			<button className='btn btn-dark' type='button'>
+				All Movies
+			</button>
+			<button className='btn btn-outline-dark' type='button'>
+				All Movies
+			</button>
+			<button className='btn btn-outline-dark' type='button'>
+				All Movies
+			</button>
 		</div>
 	)
 }
