59b0058503fc3287be856a5c96a85dc10c73b5c3
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-09 08:05:38 +0000

    Auto-backup: 9/9/2026, 8:05:37 AM - Updated my-app/src/index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/app/App'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<React.StrictMode>
+	<StrictMode>
 		<App />
-	</React.StrictMode>,
+	</StrictMode>,
 )
