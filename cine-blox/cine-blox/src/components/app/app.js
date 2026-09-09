90dbf8a85d764d68c6ea15b42ae7bab4605168cc
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-09 10:55:09 +0000

    Auto-backup: 9/9/2026, 10:55:09 AM - Updated cine-blox/cine-blox/src/components/app/app.js

--- cine-blox/cine-blox/src/components/app/app.js
+++ cine-blox/cine-blox/src/components/app/app.js
@@ -8,7 +8,7 @@ const App = () => {
 		<div className='app'>
 			<div className='content'>
 				<AppInfo />
-				<div>
+				<div className='search-panel'>
 					<SearchPanel />
 					<AppFilter />
 				</div>
