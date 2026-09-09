ea5ddf6b6976612942c896537c869a2d527b61e8
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-09 10:33:05 +0000

    Auto-backup: 9/9/2026, 10:33:04 AM - Updated cine-blox/cine-blox/src/components/app/app.js

--- cine-blox/cine-blox/src/components/app/app.js
+++ cine-blox/cine-blox/src/components/app/app.js
@@ -1,7 +1,7 @@
 import '../app/app.css'
 
 import AppFilter from '../app-filter/app-filter'
-import { AppInfo } from '../app-info/app-info'
+import AppInfo from '../app-info/app-info'
 import SearchPanel from '../search-panel/search-panel'
 
 export const App = () => {
