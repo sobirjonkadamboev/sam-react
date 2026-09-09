334d10af3332c033857681ca64105738a6606c6d
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-09 11:00:00 +0000

    Auto-backup: 9/9/2026, 11:00:00 AM - Updated cine-blox/cine-blox/src/components/app-info/app-info.js

--- cine-blox/cine-blox/src/components/app-info/app-info.js
+++ cine-blox/cine-blox/src/components/app-info/app-info.js
@@ -1,7 +1,11 @@
 import './app-info.css'
 
 const AppInfo = () => {
-	return <div className='app-info'>AppInfo</div>
+	return (
+		<div className='app-info'>
+			<p className='fs-3 text-uppercase'>Movies Watched: </p>
+		</div>
+	)
 }
 
 export default AppInfo
