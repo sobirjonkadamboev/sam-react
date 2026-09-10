ddf10372b922eecf1031c7821a98afa9b0133636
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-10 04:56:54 +0000

    Auto-backup: 9/10/2026, 4:56:54 AM - Updated testProps/app.js

---
+++ testProps/app.js
@@ -0,0 +1,17 @@
+const User = () => {
+	return (
+		<div>
+			<h1>My name is Sobirjon</h1>
+			<a href='youtube.com'>YouTube channel</a>
+		</div>
+	)
+}
+const App = () => {
+	return (
+		<div>
+			<User />
+		</div>
+	)
+}
+
+export default App
