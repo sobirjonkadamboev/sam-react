8a62116b03534659b057acaf12789721aac7a1d7
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-10 04:43:46 +0000

    Auto-backup: 9/10/2026, 4:43:46 AM - Updated cine-blox/cine-blox/src/components/movies-add-form/movies-add-form.js

--- cine-blox/cine-blox/src/components/movies-add-form/movies-add-form.js
+++ cine-blox/cine-blox/src/components/movies-add-form/movies-add-form.js
@@ -1,7 +1,11 @@
 import './movies-add-form.css'
 
 const MoviesAddForm = () => {
-	return <div className='movies-add-form'>MoviesAddForm</div>
+	return (
+		<div className='movies-add-form'>
+			<h3>Add New Movie</h3>
+		</div>
+	)
 }
 
 export default MoviesAddForm
