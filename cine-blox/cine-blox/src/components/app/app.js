ee8c563b3a60d788940de09cc64ca8500e16b5ff
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-10 04:58:30 +0000

    Auto-backup: 9/10/2026, 4:58:30 AM - Updated cine-blox/cine-blox/src/components/app/app.js

--- cine-blox/cine-blox/src/components/app/app.js
+++ cine-blox/cine-blox/src/components/app/app.js
@@ -1,22 +1,16 @@
-import AppFilter from '../app-filter/app-filter'
-import AppInfo from '../app-info/app-info'
-import '../app/app.css'
-import MovieList from '../movie-list/movie-list'
-import MoviesAddForm from '../movies-add-form/movies-add-form'
-import SearchPanel from '../search-panel/search-panel'
-
+const User = props => {
+	console.log(props)
+	return (
+		<div>
+			<h1>My name is Sobirjon</h1>
+			<a href='youtube.com'>YouTube channel</a>
+		</div>
+	)
+}
 const App = () => {
 	return (
-		<div className='app font-monospace'>
-			<div className='content'>
-				<AppInfo />
-				<div className='search-panel'>
-					<SearchPanel />
-					<AppFilter />
-				</div>
-				<MovieList />
-				<MoviesAddForm />
-			</div>
+		<div>
+			<User />
 		</div>
 	)
 }
