462e9a0cca304e30d4e5afc753787510cba3f4a6
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-11 05:29:49 +0000

    Auto-backup: 9/11/2026, 5:29:49 AM - Updated cine-blox/cine-blox/src/components/movie-list/movie-list.jsx

--- cine-blox/cine-blox/src/components/movie-list/movie-list.jsx
+++ cine-blox/cine-blox/src/components/movie-list/movie-list.jsx
@@ -5,11 +5,7 @@ const MovieList = ({ data }) => {
 	return (
 		<ul className='movie-list'>
 			{data.map(item => (
-				<MovieListItem
-					name={item.name}
-					viewers={item.viewers}
-					favourite={item.favourite}
-				/>
+				<MovieListItem {...item} />
 			))}
 		</ul>
 	)
