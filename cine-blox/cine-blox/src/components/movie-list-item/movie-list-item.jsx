43c0f6432c991700d63ca78c5271310bf55504d4
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-11 05:05:13 +0000

    Auto-backup: 9/11/2026, 5:05:13 AM - Updated cine-blox/cine-blox/src/components/movie-list-item/movie-list-item.jsx

--- cine-blox/cine-blox/src/components/movie-list-item/movie-list-item.jsx
+++ cine-blox/cine-blox/src/components/movie-list-item/movie-list-item.jsx
@@ -1,8 +1,6 @@
 import './movie-list-item.css'
 
-const MovieListItem = ({ name, viewers }) => {
-	console.log(name, viewers)
-
+const MovieListItem = ({ data }) => {
 	return (
 		<li className='list-group-item d-flex justify-content-between'>
 			<span className='list-group-item-label'>{name}</span>
