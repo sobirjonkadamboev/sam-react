0ccf27559a04e3aec4274c20a452900aa4b97fb4
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-11 04:57:37 +0000

    Auto-backup: 9/11/2026, 4:57:37 AM - Updated cine-blox/cine-blox/src/components/movie-list-item/movie-list-item.jsx

--- cine-blox/cine-blox/src/components/movie-list-item/movie-list-item.jsx
+++ cine-blox/cine-blox/src/components/movie-list-item/movie-list-item.jsx
@@ -5,11 +5,11 @@ const MovieListItem = ({ name, viewers }) => {
 
 	return (
 		<li className='list-group-item d-flex justify-content-between'>
-			<span className='list-group-item-label'>Spiderman: Brand-new Day</span>
+			<span className='list-group-item-label'>{name}</span>
 			<input
 				type='number'
 				className='list-group-item-input'
-				defaultValue='247'
+				defaultValue={viewers}
 			/>
 			<div className='d-flex justify-content-center align-items-center'>
 				<button type='button' className='btn-cookie btn-sm'>
