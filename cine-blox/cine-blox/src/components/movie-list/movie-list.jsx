dda96ae279f8531aa1c73dce5e0c6b6e39c7464e
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-11 05:10:48 +0000

    Auto-backup: 9/11/2026, 5:10:48 AM - Updated cine-blox/cine-blox/src/components/movie-list/movie-list.jsx

--- cine-blox/cine-blox/src/components/movie-list/movie-list.jsx
+++ cine-blox/cine-blox/src/components/movie-list/movie-list.jsx
@@ -4,7 +4,7 @@ import './movie-list.css'
 const MovieList = () => {
 	return (
 		<div className='movie-list'>
-			<MovieListItem name='Spiderman: Brand-new Day' viewers={107} />
+			<MovieListItem name={data[0].name} viewers={107} />
 			<MovieListItem name='Avengers: Dooms Day' viewers={357} />
 			<MovieListItem name='Dune Series' viewers={1448} />
 			<MovieListItem name='Interstellar' viewers={32505} />
